029a343eaddaef07f796a432a48a5cf9f94560ba
Hal Finkel <hfinkel@anl.gov>
2015-04-19 17:21:09 +0000

    use __BEGIN_DECLS/__END_DECLS in fixup for spi/include/kernel/process.h

--- ppcfloor-fixup/spi/include/kernel/process.h
+++ ppcfloor-fixup/spi/include/kernel/process.h
@@ -5,8 +5,12 @@
 
 #include <hwi/include/common/compiler_support.h>
 
+__BEGIN_DECLS
+
 __INLINE__
 uint64_t Kernel_GetJobID();
+
+__END_DECLS
 
 #include_next <spi/include/kernel/process.h>
 #endif
